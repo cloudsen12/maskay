916f94ccd897fdb48bd97fa9040f2750ea429b78
Gonzalo Mateo-Garcia <gonzmg88@gmail.com>
2022-09-07 12:03:20 +0000

    Fixed test

--- maskay/library/__init__.py
+++
@@ -1,6 +0,0 @@
-from .unetmobv2 import UnetMobV2
-from .KappaMaskL1C import KappaModelUNetL1C
-from .KappaMaskL2A import KappaModelUNetL2A
-from .CDFCNNrgbi import CDFCNNrgbi
-from .CDFCNNrgbiswir import CDFCNNrgbiswir
-from .dynamicworld import DynamicWorld
